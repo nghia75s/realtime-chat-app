59a4b19c8ae23bcadd705da1a6320a1ae4e52dc3
nghia75s <nghia85b@gmail.com>
2026-03-25 09:51:16 +0700

    login, logout function added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,3 +52,29 @@ export const signup = async (req, res) => {
         res.status(500).json({message: "Server error"});
     }
 };
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+
+        generateToken(user._id, res);
+        res.status(200).json({_id: user._id, fullname: user.fullname, email: user.email, profilePicture: user.profilePicture});
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({message: "Server error"});
+    }
+}
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({message: "Logged out successfully"});
+}
